9d17b4f27a17aaf86f5020f0a6ed594669496e02
Touqeer Ansari <touqeeransari001@gmail.com>
2023-11-23 01:13:28 +0530

    update app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,23 @@
 import "./App.css";
 import Home from "./componets/Pages/Home";
+import Login from "./componets/Pages/Login/Login";
+import {Routes,Route, BrowserRouter} from "react-router-dom"
 
 function App() {
   return (
-    <>
-      <Home></Home>
-    </>
+    <div className='App'>
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>}></Route>
+        <Route path='/login' element={<Login/>}></Route>
+      </Routes>
+      
+      </BrowserRouter>
+        
+        <Home></Home>
+        <Login></Login>
+     
+    </div>
   );
 }
 
