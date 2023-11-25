fa38b20de1f2b96e51db62abd7fed832e556ad06
Touqeer Ansari <touqeeransari001@gmail.com>
2023-11-25 18:29:01 +0530

    Update app .js

--- client/src/App.js
+++ client/src/App.js
@@ -9,13 +9,11 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />}></Route>
+          <Route path="/" element={<Home></Home>}></Route>
           <Route path="/login" element={<Login />}></Route>
           <Route path="/signup" element={<Signup></Signup>}></Route>
         </Routes>
       </BrowserRouter>
-
-     
     </div>
   );
 }
