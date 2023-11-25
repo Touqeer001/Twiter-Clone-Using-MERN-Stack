61d512193f7c44caf29319c284e90015f1b657d1
Touqeer Ansari <touqeeransari001@gmail.com>
2023-11-25 18:29:22 +0530

    update login js

--- client/src/componets/Pages/Login/Login.jsx
+++ client/src/componets/Pages/Login/Login.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import GoogleButton from "react-google-button";
+import { useSignInWithGoogle } from "react-firebase-hooks/auth";
 
 // imoprt twiterImage from '../Image/twiterImage.jpg';
 import twiterImage from "../Image/twitter.jpeg";
@@ -8,48 +10,57 @@ import auth from "../../../firebase.init";
 import TwitterIcon from "@mui/icons-material/Twitter";
 // import TwitterIcon from "@mui/icons-material/Twitter";
 import { useSignInWithEmailAndPassword } from "react-firebase-hooks/auth";
+import "./style.css";
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  // const [ setError] = useState("");
+  // const { logIn, googleSignIn } = useUserAuth();
+  const navigate = useNavigate();
 
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log(email, password);
-    signInWithEmailAndPassword(email, password)}
-  
+    signInWithEmailAndPassword(email, password);
+  };
+
+  const handleLogin = () => {
+    // e.preventDefault();
+
+    signInWithGoogle();
+  };
+
+  const [signInWithGoogle, gooleUser, googleLoading, googleError] =
+    useSignInWithGoogle(auth);
+
   const [signInWithEmailAndPassword, user, loading, error] =
     useSignInWithEmailAndPassword(auth);
   console.log(user);
-  if (error) {
-    return (
-      <div>
-        <p>Error: {error.message}</p>
-      </div>
-    );
+  console.log(gooleUser);
+  if (error || googleError) {
+    console.log("googleErro");
+    console.log("googleErro");
   }
-  if (loading) {
+  if (loading || googleLoading) {
     return <p>Loading...</p>;
   }
-  if (user) {
-    return (
-      <div>
-        <p>Signed In User: {user.email}</p>
-      </div>
-    );
+  if (user || gooleUser) {
+    console.log("gooleUser");
+    navigate("/");
   }
 
   return (
     <>
       <div className="login-container">
-        <div image-container>
+        <div className="image-container">
           <img className="image" src={twiterImage} alt="Twiter Iamge"></img>
         </div>
         <div className="form-container">
           <div className="form-box">
-            <TwitterIcon></TwitterIcon>
+            <TwitterIcon className="Twitter-img"></TwitterIcon>
 
-            <h2 className="heading">Happening Now</h2>
+            <h1 className="heading-1">Happening Now</h1>
             <form onSubmit={handleSubmit}>
               <input
                 className="email"
@@ -58,23 +69,40 @@ const Login = () => {
                 onChange={(e) => setEmail(e.target.value)}
               />
               <input
+                className="email"
                 type="password"
                 placeholder="Pasword"
                 onChange={(e) => setPassword(e.target.value)}
               ></input>
               <div className="btn-login">
-                <button className="bt-submit" type="submit">
+                <button className="btn" type="submit">
                   Log in
                 </button>
               </div>
             </form>
             <hr></hr>
           </div>
-          <div> Goole Button</div>
           <div>
+            {" "}
+            <GoogleButton
+              className="g-btn"
+              type="light"
+              onClick={handleLogin}
+            ></GoogleButton>
+          </div>
+          <div className="signUpText">
             Dont Have Account
-            <Link to="/signup">Sign Up</Link>
-          
+            <Link
+              to="/signup"
+              style={{
+                textDecoration: "none",
+                color: "var(--twitter-color)",
+                fontWeight: "600",
+                marginLeft: "5px",
+              }}
+            >
+              Sign Up
+            </Link>
           </div>
         </div>
       </div>
