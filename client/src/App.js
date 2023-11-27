95c2b6b800d15020776bce382d85fd53ad1d06c0
Touqeer Ansari <touqeeransari001@gmail.com>
2023-11-27 20:26:09 +0530

    Update

--- client/src/App.js
+++ client/src/App.js
@@ -3,15 +3,18 @@ import Home from "./componets/Pages/Home";
 import Login from "./componets/Pages/Login/Login";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Signup from "./componets/Pages/Login/Signup";
+import ProtectedRoute from "./componets/Pages/ProtectedRoute";
+import PageLoading from "./PageLoading";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home></Home>}></Route>
+          <Route path="/" element={<ProtectedRoute><Home></Home></ProtectedRoute>}></Route>
           <Route path="/login" element={<Login />}></Route>
           <Route path="/signup" element={<Signup></Signup>}></Route>
+          <Route path="/loading-page" element={<PageLoading></PageLoading>}></Route>
         </Routes>
       
       </BrowserRouter>
