92a780f4e95f647e9e9d16be8964e2aa67f8a697
Touqeer Ansari <touqeeransari001@gmail.com>
2023-11-27 20:26:33 +0530

    Update

--- client/src/componets/Pages/Home.jsx
+++ client/src/componets/Pages/Home.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import Feed from "./Feed/Feed";
+import Sidebar from "./Sidebaar/Sidebar";
+import Widgets from "./Widgets/Widgets";
 
 const Home = () => {
-  return <>This is Home Page....</>;
+  return (
+    <>
+      <Sidebar></Sidebar>
+
+      <Feed></Feed>
+      <Widgets></Widgets>
+    </>
+  );
 };
 
 export default Home;
