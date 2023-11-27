70e768f3c0a79815d6dd530ef647e0b252c6bbc4
Touqeer Ansari <touqeeransari001@gmail.com>
2023-11-28 01:40:35 +0530

    Added

---
+++ client/src/componets/Pages/Explore/Explore.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../Page.css";
+
+const Explore = () => {
+  return (
+    <div className="page">
+      <h2 className="pageTitle">Welcome to Explore Page</h2>
+    </div>
+  );
+};
+
+export default Explore;
