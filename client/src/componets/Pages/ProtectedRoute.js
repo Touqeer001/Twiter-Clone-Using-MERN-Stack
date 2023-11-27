475d080d10963f6c09cc81ebb5e0ee7806c08c2f
Touqeer Ansari <touqeeransari001@gmail.com>
2023-11-27 20:26:38 +0530

    Update

--- client/src/componets/Pages/ProtectedRoute.js
+++ client/src/componets/Pages/ProtectedRoute.js
@@ -1,17 +1,19 @@
-import React from 'react'
-import { useAuthState } from 'react-firebase-hooks/auth'
-import auth from '../../firebase.init'
-import { Navigate } from 'react-router-dom';
+import React from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import auth from "../../firebase.init";
+import { Navigate } from "react-router-dom";
+import PageLoading from "../../PageLoading";
 
-const ProtectedRoute = ({children}) => {
-    const[user,isLoading]=useAuthState(auth);
-    if(!user){
-        return <Navigate></Navigate>
-    }
+const ProtectedRoute = ({children }) => {
+  const [user, isLoading] = useAuthState(auth);
+ 
+  if (isLoading) {
+    return <PageLoading></PageLoading>;
+  }
+  if (!user) {
+    return <Navigate to="/login"></Navigate>;
+  }
+  return children;
+};
 
-  return (
-    <div>ProtectedRoute</div>
-  )
-}
-
-export default ProtectedRoute
+export default ProtectedRoute;
