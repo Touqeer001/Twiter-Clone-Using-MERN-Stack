cf2e010811ea9fc3f294fef81bd4728139bc1d39
Touqeer Ansari <touqeeransari001@gmail.com>
2023-11-28 01:40:15 +0530

    update

--- client/src/componets/Pages/Home.jsx
+++ client/src/componets/Pages/Home.jsx
@@ -2,15 +2,21 @@ import React from "react";
 import Feed from "./Feed/Feed";
 import Sidebar from "./Sidebaar/Sidebar";
 import Widgets from "./Widgets/Widgets";
+import auth from "../../firebase.init";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { signOut } from "firebase/auth";
 
 const Home = () => {
+  const user=useAuthState(auth);
+  const handleLogout = () => {
+    signOut(auth);
+  };
   return (
-    <>
-      <Sidebar></Sidebar>
+    <div>
+      <Sidebar handleLogout={handleLogout} user={user}></Sidebar>
 
-      <Feed></Feed>
       <Widgets></Widgets>
-    </>
+    </div>
   );
 };
 
