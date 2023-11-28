976127e0d8e7fe990fd7fb1c61938d1cd1afc822
Touqeer Ansari <touqeeransari001@gmail.com>
2023-11-28 15:34:24 +0530

    update

--- client/src/componets/Pages/Profile/Profile.jsx
+++ client/src/componets/Pages/Profile/Profile.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import "../Page.css";
 
 const Profile = () => {
   return (
-    <div>Profile</div>
+   
+    <div className="page">
+    <h2 className="pageTitle">Welcome to More Page</h2>
+  </div>
   )
 }
 
