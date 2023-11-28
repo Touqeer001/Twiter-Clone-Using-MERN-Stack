55d63b752a3863057bfb1ef4bf2f3193f2864ba9
Touqeer Ansari <touqeeransari001@gmail.com>
2023-11-28 22:52:33 +0530

    initial commits

---
+++ Backend/index.js
@@ -0,0 +1,74 @@
+// import express from "express";
+// import cors from "cors";
+// import bodyParser from "body-parser";
+// import Connection from "./databse/db.js";
+
+// const app = express();
+// app.use(cors());
+
+// const PORT = 8000;
+
+// app.use(bodyParser.json({ extended: true })); //ye karne se payload me body show hone lagi gi..
+// app.use(bodyParser.urlencoded({ extended: true }));
+
+// Connection();
+
+// app.listen(PORT, () => console.log(`SErver is Runnning on ${PORT}`));
+
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://touqeer:Ansari@cluster0.yimeney.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const postCollection = client.db("database").collection("posts"); // this collection is for team-ekt
+    const userCollection = client.db("database").collection("users"); // this collection is for team-srv
+
+    // get
+    app.get("/post", async (req, res) => {
+      const post = await postCollection.find().toArray();
+      res.send(post);
+    });
+    app.get("/user", async (req, res) => {
+      const user = await userCollection.find().toArray();
+      res.send(user);
+    });
+
+    app.post("/post", async (req, res) => {
+      const post = req.body;
+      const result = await postCollection.insertOne(post);
+      res.send(result);
+    });
+
+    app.post("/register", async (req, res) => {
+      const user = req.body;
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
+    
+  } catch (error) {
+    console.log(error);
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Hello from Twitter Clone!");
+});
+
+app.listen(port, () => {
+  console.log(`Twitter clone is listening on port ${port}`);
+});
