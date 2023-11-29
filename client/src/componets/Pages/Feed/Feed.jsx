8e89807b7af556ca685b96ebe40ea3dcf1dc8961
Touqeer Ansari <touqeeransari001@gmail.com>
2023-11-29 23:13:16 +0530

    updated

--- client/src/componets/Pages/Feed/Feed.jsx
+++ client/src/componets/Pages/Feed/Feed.jsx
@@ -1,14 +1,27 @@
-import React from "react";
 import "./Feed.css";
+import Post from "./Post/Post";
 import TwiterBox from "./TwiterBox/TwiterBox";
+import React, { useEffect, useState } from "react";
 
 const Feed = () => {
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:5000/post")
+      .then((res) => res.json())
+      .then((data) => {
+        setPosts(data);
+        // console.log(data);
+      });
+  }, [posts]);
   return (
     <div className="feed">
       <div className="feed__header">
         <h2 id="heading-2">Home</h2>
       </div>
       <TwiterBox></TwiterBox>
+      {posts.map((p) => (
+        <Post key={p._id} p={p}></Post>
+      ))}
     </div>
   );
 };
