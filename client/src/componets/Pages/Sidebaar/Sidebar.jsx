cfdaff9de68a94f9b6e4c57b78460c69d750671a
Touqeer Ansari <touqeeransari001@gmail.com>
2023-11-29 23:14:17 +0530

    updated

--- client/src/componets/Pages/Sidebaar/Sidebar.jsx
+++ client/src/componets/Pages/Sidebaar/Sidebar.jsx
@@ -17,13 +17,18 @@ import IconButton from "@mui/material/IconButton";
 import Menu from "@mui/material/Menu";
 import MenuItem from "@mui/material/MenuItem";
 import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
-import DoneIcon from '@mui/icons-material/Done';
-import Divider from '@mui/material/Divider';
+import DoneIcon from "@mui/icons-material/Done";
+import Divider from "@mui/material/Divider";
 
 import CustomeLink from "./CustomeLink";
-const Sidebar = ({handleLogout, user}) => {
+import useLoggedInUser from "../../../hooks/useLoggedInUser";
+const Sidebar = ({ handleLogout, user }) => {
   const [anchorEl, setAnchorEl] = useState(null);
   const openMenu = Boolean(anchorEl);
+  const [loggedInUser] = useLoggedInUser();
+  const userProfilePic = loggedInUser[0]?.profileImage
+    ? loggedInUser[0]?.profileImage
+    : "https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_960_720.png";
 
   const handleClose = () => {
     setAnchorEl(null);
@@ -32,30 +37,43 @@ const Sidebar = ({handleLogout, user}) => {
     setAnchorEl(e.currentTarget);
     console.log(e.currentTarget);
   };
+  const result = user[0]?.email?.split('@')[0];
   return (
     <div className="sidebar">
       <TwitterIcon className="sidebar__twitterIcon"></TwitterIcon>
       <CustomeLink to="/home/feed">
-      <SidebarOption active={true} text="Home" Icon={HomeIcon} /></CustomeLink>
+        <SidebarOption active={true} text="Home" Icon={HomeIcon} />
+      </CustomeLink>
       <CustomeLink to="/home/explore">
-      <SidebarOption active={true} text=" Explore" Icon={SearchIcon} /></CustomeLink>
+        <SidebarOption active={true} text=" Explore" Icon={SearchIcon} />
+      </CustomeLink>
       <CustomeLink to="/home/notification">
-      <SidebarOption
-        active={true}
-        text=" Notifications "
-        Icon={NotificationsNoneIcon}
-      /></CustomeLink>
+        <SidebarOption
+          active={true}
+          text=" Notifications "
+          Icon={NotificationsNoneIcon}
+        />
+      </CustomeLink>
       <CustomeLink to="/home/message">
-      <SidebarOption active={true} text=" Message " Icon={MailOutlineIcon} /></CustomeLink>
+        <SidebarOption active={true} text=" Message " Icon={MailOutlineIcon} />
+      </CustomeLink>
       <CustomeLink to="/home/bookmarks">
-      <SidebarOption active={true} text="Bookmarks " Icon={BookmarkBorderIcon} /></CustomeLink>
+        <SidebarOption
+          active={true}
+          text="Bookmarks "
+          Icon={BookmarkBorderIcon}
+        />
+      </CustomeLink>
       <CustomeLink to="/home/list">
-      <SidebarOption active={true} text="List " Icon={ListAltIcon} /></CustomeLink>
+        <SidebarOption active={true} text="List " Icon={ListAltIcon} />
+      </CustomeLink>
       <CustomeLink to="/home/profile">
-      <SidebarOption active={true} text=" Profile " Icon={PermIdentityIcon} /></CustomeLink>
+        <SidebarOption active={true} text=" Profile " Icon={PermIdentityIcon} />
+      </CustomeLink>
       <CustomeLink to="/home/more">
-      <SidebarOption active={true} text=" More  " Icon={MoreIcon} /></CustomeLink>
-      
+        <SidebarOption active={true} text=" More  " Icon={MoreIcon} />
+      </CustomeLink>
+
       <Button variant="outlined" className="sidebar__tweet" fullWidth>
         Tweet
       </Button>
@@ -63,12 +81,13 @@ const Sidebar = ({handleLogout, user}) => {
 
       {/* //Profile Section */}
       <div className="Profile__info">
-        <Avatar src="https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_960_720.png" />
+        {/* <Avatar src="https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_960_720.png" /> */}
+        <Avatar src={userProfilePic}  />
         <div className="user__info">
-          <h4>Touqeer Ansari</h4>
-          <h5>@touqeer</h5>
+          <h4> {loggedInUser[0]?.name ? loggedInUser[0].name : user && user[0].displayName}</h4>
+          <h5>@{result}</h5>
         </div>
-       
+
         <IconButton
           style={{ marginLeft: 2 }}
           aria-controls={openMenu ? "basic-menu" : undefined}
@@ -82,19 +101,20 @@ const Sidebar = ({handleLogout, user}) => {
           id="basic-menu"
           anchorEl={anchorEl}
           open={openMenu}
-          onClick={handleClose} 
+          onClick={handleClose}
           onClose={handleClose}
         >
           <MenuItem className="Profile__info1">
-            <Avatar src="https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_960_720.png" />
+          <Avatar src={loggedInUser[0]?.profileImage ? loggedInUser[0]?.profileImage : "https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_960_720.png"} />
             <div className="user__info subUser__info">
-                <div>
-              <h4>Touqeer Ansari</h4>
-              <h5>@touqeer</h5>
+              <div>
+                <h4>{loggedInUser[0]?.name ? loggedInUser[0].name : user && user[0].displayName}</h4>
+                <h5>@{result}</h5>
               </div>
-              <ListItemIcon className="done__icon" color="blue"><DoneIcon /></ListItemIcon>
+              <ListItemIcon className="done__icon" color="blue">
+                <DoneIcon />
+              </ListItemIcon>
             </div>
-            
           </MenuItem>
           <Divider />
           <MenuItem onClick={handleClose}>Add an existing account</MenuItem>
