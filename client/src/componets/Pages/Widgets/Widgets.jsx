377c6a64e5717370b48206b699ff02205a805d82
Touqeer Ansari <touqeeransari001@gmail.com>
2023-11-29 23:14:27 +0530

    updated

--- client/src/componets/Pages/Widgets/Widgets.jsx
+++ client/src/componets/Pages/Widgets/Widgets.jsx
@@ -1,9 +1,35 @@
 import React from "react";
+import {
+  TwitterTimelineEmbed,
+  TwitterTweetEmbed,
+} from "react-twitter-embed";
+import SearchIcon from "@mui/icons-material/Search";
 
 import "./Widgets.css";
 
 const Widgets = () => {
-  return <div className="widgets">Widgets</div>;
+  return (
+    <div className="widgets">
+      <div className="widgets__input">
+        <SearchIcon className="widgets__searchIcon" />
+        <input placeholder="Search Twitter" type="text" />
+      </div>
+
+      <div className="widgets__widgetContainer">
+        <h2>What's happening</h2>
+
+        <TwitterTweetEmbed tweetId={"1557187138352861186"} />
+
+        <TwitterTimelineEmbed
+          sourceType="profile"
+          screenName="elonmusk"
+          options={{ height: 400 }}
+        />
+
+
+      </div>
+    </div>
+  );
 };
 
 export default Widgets;
