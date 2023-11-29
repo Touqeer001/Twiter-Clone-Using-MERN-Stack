ad2d403e24a382998265645ebc5db9d79a88d0e9
Touqeer Ansari <touqeeransari001@gmail.com>
2023-11-29 22:47:29 +0530

    updated

--- Backend/index.js
+++ Backend/index.js
@@ -39,13 +39,18 @@ async function run() {
 
     // get
     app.get("/post", async (req, res) => {
-      const post = await postCollection.find().toArray();
+      const post = (await postCollection.find().toArray()).reverse();
       res.send(post);
     });
     app.get("/user", async (req, res) => {
       const user = await userCollection.find().toArray();
       res.send(user);
     });
+    app.get('/loggedInUser', async (req, res) => {
+        const email = req.query.email;
+        const user = await userCollection.find({ email: email }).toArray();
+        res.send(user);
+    })
 
     app.post("/post", async (req, res) => {
       const post = req.body;
@@ -58,7 +63,17 @@ async function run() {
       const result = await userCollection.insertOne(user);
       res.send(result);
     });
-    
+
+       // patch
+       app.patch('/userUpdates/:email', async (req, res) => {
+        const filter = req.params;
+        const profile = req.body;
+        const options = { upsert: true };
+        const updateDoc = { $set: profile };
+        const result = await userCollection.updateOne(filter, updateDoc, options);
+        res.send(result)
+    })
+
   } catch (error) {
     console.log(error);
   }
